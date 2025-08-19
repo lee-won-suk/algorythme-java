617a4fe449f503e5f7f737867aa162c4ce5c768e
leewonsuk <tjdjdjrdl123@naver.com>
2025-08-19 23:17:12 +0900

    feat: 문제풀이 추가

--- src/백준/구현/알람시계2884.java
+++ src/백준/구현/알람시계2884.java
@@ -4,6 +4,9 @@ package 백준.구현;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.StringTokenizer;
 
 public class 알람시계2884 {
@@ -14,21 +17,11 @@ public class 알람시계2884 {
         int hour = Integer.parseInt(st.nextToken());
         int minute = Integer.parseInt(st.nextToken());
 
-        if (minute >= 45) {
-            System.out.printf("%d %d", hour, minute - 45);
-        }
-
-        if (minute < 45) {
-            if (hour == 0) {
-                hour = hour + 23;
-            } else {
-                hour -= 1;
-            }
-            minute = minute + 15;
-            System.out.printf("%d %d", hour, minute);
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H m");
+        LocalTime a = LocalTime.of(hour, minute);
+        a = a.minusMinutes(45);
+        System.out.println(a.format(formatter));
         br.close();
-
 
     }
 
